4830c2fc721e901cb280e607a82f1a1708b28940
Priyanka <priyankasingla221@gmail.com>
2022-02-05 20:48:41 -0400

    updated assignment

--- src/banquetHall/Banquet.java
+++ src/banquetHall/Banquet.java
@@ -1,6 +1,5 @@
 package banquetHall;
 
-
 public class Banquet {
 	double baseCost;
 	double foodCost;
@@ -11,71 +10,62 @@ public class Banquet {
 	double cesstaxfortotalbasecost;
 	double noofguest;
 
-	public Banquet(double baseCost, double foodCost, double beveragesCost, double tip) {
-		
-		this.foodCost = foodCost;
-		this.beveragesCost = beveragesCost;
-		this.tip = tip;
-	}
-
 	double calculateBaseCost(double totalBaseCost) {
-		// Assuming that there are 3 package offered by carmens banquet hall starts from base  Charish for 2000$, beloved 3000$, Harmony 4000
-		//values entered other than these will not give calculation result.
+		// Assuming that there are 3 package offered by carmens banquet hall starts from
+		// base Charish for 2000$, beloved 3000$, Harmony 4000
+		// values entered other than these will not give calculation result.
 		totalbasecost = totalBaseCost;
-		if (baseCost==2000 || baseCost == 3000 || baseCost ==4000 ) {
-		
 		totalbasecost = baseCost + foodCost + beveragesCost + tip;
-		
-		}
-		else {
-			System.out.println("Invalid base amount! Please choose one of the base prices listed above.");
-			
-		}
 		return totalbasecost;
 
 	}
 
-	double calculatetax(double taxForTotalBaseCost) {
-
+	private double calculatetax(double taxForTotalBaseCost) {
+		double callingcalcatebasecost = calculateBaseCost(totalbasecost);
 		taxfortotalbasecost = taxForTotalBaseCost;
 		taxfortotalbasecost = totalbasecost * 0.15;
-		System.out.println("Provincial HST/GST 15%");
+		// System.out.println("Provincial HST/GST 15%");
 		return taxfortotalbasecost;
 	}
 
-	double calculateCess(double noOfGuest, double cessTaxForTotalBaseCost) {
+	private double calculateCess(double noOfGuest, double cessTaxForTotalBaseCost) {
+		double callingcalculatetax = calculatetax(taxfortotalbasecost);
+		double callingcalculatebasecost = calculateBaseCost(totalbasecost);
 		noOfGuest = noofguest;
 		cesstaxfortotalbasecost = cessTaxForTotalBaseCost;
 
 		// Less than or equal to 40 guests, cess is 4%
 		if (noOfGuest <= 40) {
-			System.out.println("Cess Fee Charged 4%");
+			// System.out.println("Cess Fee Charged 4%");
 			cesstaxfortotalbasecost = totalbasecost * 0.04;
 		}
 
 		// More than 40 and up to 80 guests, cess is 8%
 		else if (noOfGuest > 40 && noOfGuest <= 80) {
-			System.out.println("Cess Fee Charged 8%");
+			// System.out.println("Cess Fee Charged 8%");
 			cesstaxfortotalbasecost = totalbasecost * 0.08;
 		}
 
 		// For more than 80 and less than 150 guests, cess is 10%
 		else if (noOfGuest > 80 && noOfGuest < 150) {
-			System.out.println("Cess Fee Charged 10%");
+			 //System.out.println("Cess Fee Charged 10%");
 			cesstaxfortotalbasecost = totalbasecost * 0.10;
 		}
 
 		// For more than 150 guests, cess will be 12.5%
-		else  {
-			System.out.println("Cess Fee Charged 12.5%");
+		else {
+			// System.out.println("Cess Fee Charged 12.5%");
 			cesstaxfortotalbasecost = totalbasecost * 0.125;
-
 		}
-		
+
 		return cesstaxfortotalbasecost;
 	}
 
 	double calculateTheTotalCost(double calculatetotalcost) {
+
+		double callingbasecost = calculateBaseCost(totalbasecost);
+		double callingtax = calculatetax(taxfortotalbasecost);
+		double callingcesstax = calculateCess(callingbasecost, callingtax);
 		calculatetotalcost = totalbasecost + taxfortotalbasecost + cesstaxfortotalbasecost;
 		return calculatetotalcost;
 
--- src/banquetHall/MainBanquet.java
+++ src/banquetHall/MainBanquet.java
@@ -9,29 +9,36 @@ public class MainBanquet {
 
 		Scanner sc = new Scanner(System.in);
 
-		Banquet obj = new Banquet(0, 0, 0, 0);
-		System.out.println("Welcome to Carmens!" +"\n"+ " We Offer 3 Type of Packages:" +"\n"+"-Cherish : 2000" +"\n"+"-Beloved : 3000" +"\n" +"-Harmoney : 4000");
+		Banquet obj = new Banquet();
+
+		System.out.println("Welcome to Carmens!" + "\n" + " We Offer 3 Type of Packages:" + "\n" + "-Cherish : 2000"
+				+ "\n" + "-Beloved : 3000" + "\n" + "-Harmoney : 4000");
+
 		System.out.println("Please Enter Base Cost of the Package you wish to Book: ");
 		obj.baseCost = sc.nextDouble();
-		
-		
-		System.out.println("Please Enter Food Cost: ");
-		obj.foodCost = sc.nextDouble();
+		if (obj.baseCost == 2000 || obj.baseCost == 3000 || obj.baseCost == 4000) {
+			System.out.println("Please Enter Food Cost: ");
+			obj.foodCost = sc.nextDouble();
+			System.out.println("Please Enter Beverages Cost: ");
+			obj.beveragesCost = sc.nextDouble();
+			System.out.println("Please Enter Tip: ");
+			obj.tip = sc.nextDouble();
 
-		System.out.println("Please Enter Beverages Cost: ");
-		obj.beveragesCost = sc.nextDouble();
+			System.out.println("Please enter Number of Guests: ");
+			obj.noofguest = sc.nextInt();
 
-		System.out.println("Please Enter Tip: ");
-		obj.tip = sc.nextDouble();
+			if (obj.noofguest != 0) {
 
-		System.out.println("Please enter Number of Guests: ");
-		obj.noofguest = sc.nextInt();
+				System.out.println("Base Cost Before Taxes: " + obj.calculateBaseCost(0));
+				// System.out.println("Cess Charges: " + obj.calculateCess(0, 0));
+				// System.out.println("HST/GST Calculated Taxes: " + obj.calculatetax(0));
+				System.out.println("Total Booking cost After Taxes: " + obj.calculateTheTotalCost(0));
+			} else {
+				System.out.println("oops! Invalid number of guest");
+			}
+		} else {
+			System.out.println("Invalid package amount!");
+		}
 
-		
-		System.out.println("Base Cost Before Taxes: " + obj.calculateBaseCost(0));
-		System.out.println("HST/GST Calculated Taxes: " + obj.calculatetax(0));
-		System.out.println("Cess Charges: " + obj.calculateCess(0, 0));
-		System.out.println("Total Booking cost: " + obj.calculateTheTotalCost(0));
 	}
-
 }
